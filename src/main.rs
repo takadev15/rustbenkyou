41958705614a72b781f288264ab5a7bc7eaa3445
Daffa Haryadi <daffa.hp.dh@gmail.com>
2021-02-07 23:29:26 +0700

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 10;
+    println!("{ }", x);
+
+    // this is a comment
+
+    let y: f64 = 2.121212;
+    println!("{ }", y);
+
+    let a;
+    a = 0;
+    println!("{ }", a);
+
+    let q = a + x;
+    println!("{ }", q);
 }
